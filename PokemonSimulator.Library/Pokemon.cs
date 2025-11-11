610729bef69e5465c7197ea15d8b2628f6de3ec5
Christopher Clemons (Windows) <chris.clemons85@gmail.com>
2025-11-11 19:09:20 +0100

    add `Pokemon` abstract class

---
+++ PokemonSimulator.Library/Pokemon.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonSimulator.Library
+{
+    internal abstract class Pokemon(string name, ElementType type, List<Attack> attacks) : IEvolvable
+    {
+        public string Name { get; private set; } = name;
+        public int Level { get; private set; } = 1;
+        public ElementType Type { get; } = type;
+
+        private List<Attack> Attacks { get; } = attacks;
+        private readonly Random _random = new Random();
+
+        public void Evolve()
+        {
+            Level += 10;
+            Console.WriteLine($"{Name} is evolving...");
+            // ToDo: Change name
+            // Name = newName
+            Console.WriteLine($"Now it is a{Name}and its level is {Level}");
+        }
+
+        public void RandomAttack()
+        {
+            int randomIndex = _random.Next(0, Attacks.Count);
+            Attacks[randomIndex].Use(Level);
+        }
+
+        public void Attack() {
+            // ToDo: Get attack by name
+        }
+
+        public void RaiseLevel() {
+            Level++;
+            Console.WriteLine($"{Name} has leveled up! {Name} is now at level {Level}.");
+        }
+    }
+}
