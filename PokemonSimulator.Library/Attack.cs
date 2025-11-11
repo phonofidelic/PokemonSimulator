0f01b2ce0526ad18c9eaa2aa3268a59c3888db24
Christopher Clemons (Windows) <chris.clemons85@gmail.com>
2025-11-11 17:25:27 +0100

    add `Attack` class

---
+++ PokemonSimulator.Library/Attack.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PokemonSimulator.Library
+{
+    internal class Attack(string name, ElementType type, int basePower)
+    {
+        public string Name { get; } = name;
+        public ElementType Type { get; } = type;
+        private int BasePower { get; } = basePower;
+
+        public void Use(int level) {
+            Console.WriteLine($"{Name} hit with a total power of {BasePower + level}");
+        }
+    }
+}
